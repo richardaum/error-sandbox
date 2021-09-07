56663c8dda5ef8b56bf0fa780b30900ac4b8af57
Richard <richard.lopes92@gmail.com>
2021-09-06 21:02:15 -0300

    remove react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,37 @@
-import axios from "axios";
 import React from "react";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      cacheTime: 30000,
-      staleTime: Infinity,
-      retry: 0,
-    },
-  },
-});
+const useQuery = () => {
+  const [isError, setError] = React.useState(false);
+  const [data, setData] = React.useState({ myAwesomeProperty: [] });
 
-const useMyQuery = () =>
-  useQuery(
-    "period",
-    async () => {
-      const { data } = await axios.get(`http://http.us/500`);
-      return data;
-    },
-    { placeholderData: { aggregations: [] } }
-  );
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setError(true);
+      setData(undefined);
+    }, 0);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return { data, isError };
+};
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Parent />
-    </QueryClientProvider>
-  );
+  return <Parent />;
 };
 
 const Parent = () => {
-  const { isError } = useMyQuery();
-  console.log("Parent", JSON.stringify({ isError }));
+  const { isError } = useQuery();
   return isError ? <Error /> : <Child />;
 };
 
 const Child = () => {
-  const { data: periods, isError } = useMyQuery();
-  console.log("Child", JSON.stringify({ periods, isError }));
+  const { isError, data } = useQuery();
+
+  if (isError) return null;
+
   return (
     <pre>
-      <code>{JSON.stringify(periods.aggregations)}</code>
+      <code>{JSON.stringify(data.myAwesomeProperty)}</code>
     </pre>
   );
 };
